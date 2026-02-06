6e088e7a440c9c8f6b80200a20c1937bb5271302
Joddd0311 <nishant0311111@gmail.com>
2026-02-06 20:05:12 +0530

    Models

--- internal/cart/dto/user.go
+++
@@ -1,6 +0,0 @@
-package dto
-
-type User struct {
-	ID    string `json:"id"`
-	Email string `json:"email"`
-}
--- internal/order/dto/product.go
+++
@@ -1,8 +0,0 @@
-package dto
-
-type Product struct {
-	ID    string  `json:"id"`
-	Code  string  `json:"code"`
-	Name  string  `json:"name"`
-	Price float64 `json:"price"`
-}
